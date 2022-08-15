14035f7e600e8af3fe0560297715e7b39cce65a6
JPD99 <jeremedoan@gmail.com>
2022-08-15 00:41:32 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -4,29 +4,27 @@
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
 
-// Use test driven development to complete the following questions
+// *********  Use test driven development to complete the following questions *********
 // Add appropriate dependencies: $ yarn add jest
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
-
 // a) Create a test with expect statements for each of the variables provided.
-
-const numbersArray1 = [6, 7, 8, 9, 10]
+// const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
-
-
 // b) Create the function that makes the test pass.
+// DO NOT ATTEMPT THE INSTRUCTOR'S EXAMPLE
+
+
+
 
 
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
-
-// a) Create a test with expect statements for each of the variables provided.
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -35,29 +33,126 @@ const object2 =  { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
+// a) Create a test with expect statements for each of the variables provided.
+//PseudoCode: 
+// Declaring a function called divisiblity
+// Parameter is an object, trying to extract the property from the object,,,,,,,, using dot notation to grab our key value pairs
+// Output will refer to one of the keypairs and a string that says if the number is divisible by 3 or not 
+
+// As we are testing we need to describe a inert function that is not defined ==> yarn jest will trigger giving us controlled fail status
+// It statement says what our function is gonna do 
+// Expect statements are going to account for ALL of our conditions from const object1-3
+describe ("divisiblity", () => {
+    it ("Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3", () => {
+        expect (divisiblity(object1)).toEqual(`${object1.number} is divisble by three`)
+        expect (divisiblity(object2)).toEqual(`${object2.number} is divisble by three`)
+        expect (divisiblity(object3)).toEqual(`${object3.number} is NOT divisble by three`)
+    })
+})
+
+//  Good Failure:
+//  FAIL  ./code-challenges.test.js
+//   divisiblity
+//   ✕ Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3 (2 ms)
+// ● divisiblity › Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3
+//   ReferenceError: divisiblity is not defined
 
 // b) Create the function that makes the test pass.
+// Creating the TEST
+// We already addressed that the parameter will be inputted as an object with thier respective names.
+// We need a new container that will grab the property called number in each object;;;;;; we will be using the objectNumGrabber to store the property called number inside of it
+// A data type of a number should only be stored in the objectNumGrabber we will use modulo (%) to identify divisibility of some number
+// create a decision tree (conditional statement) that will logically choose if a number is divisble or not and then execute the command that will print if its divisble or not
+const divisiblity = (object) => {
+    let objectNumGrabber = object.number 
+    if (objectNumGrabber % 3 === 0){
+        return `${objectNumGrabber} is divisble by three`
+    } else {
+        return `${objectNumGrabber} is NOT divisble by three`
+    } 
+}
+// SUCCESS!!!!!!  
+// PASS  ./code-challenges.test.js
+//   divisiblity
+//   ✓ Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3 (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+
+
+
 
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
-
-// a) Create a test with expect statements for each of the variables provided.
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
+// a) Create a test with expect statements for each of the variables provided.
+//PseudoCode: 
+// Declare a function called capitalize
+// Input / parameter is an array of lowercase words
+// The function should be capable of only capitalizing the first letter of each string
+// Output should produce another array, altering the first letter of each string
+
+describe ("capitalize", () => {
+    it ("Input array is lowercase strings, Output should be an array with every first letter capitalize", () => {
+        expect (capitalize(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect (capitalize(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
+
+// Good Failure:
+// FAIL  ./code-challenges.test.js
+// divisiblity
+//   ✓ Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3 (2 ms)
+// capitalize
+//   ✕ Input array is lowercase strings, Output should be an array with every first letter capitalize
+// vowelFinder
+//   ✓ Inputted is a string, Output is the index of the FIRST vowel (1 ms)
+// ● capitalize › Input array is lowercase strings, Output should be an array with every first letter capitalize
+//   ReferenceError: capitalize is not defined
 
 // b) Create the function that makes the test pass.
+// Create a function called capitalize
+// The methods: toUpperCase(), .split(), and .join() come to mind; .toUpperCase() turns strings to capslock temporary modifier, .split() turns an string to an array--acessor, and .join() turns an array into a string-acessor
+// This one took a bit long... an additional method added in notes is: .substring() ==> this method outputs A STRING ONLY, whatever is in the parameters of .substring is ommited out of the answer; the odd thing is that .substring starts at 1 and continues on.
+// Example let str = "apple" 
+// with str.substring(1) ==> console.log (str.substring(1)) ==> "pple"
+const capitalize = (array) => {
+    let copyArray = array
+    let containerArray = []
+// forloop that iterates through all elements in a array
+    for (let index = 0; index < array.length; index = index + 1) {
+//Explaination of line 132. All of the actions are pushed into the container array called containerArray: AT INDEX 0 for randomNouns1 ==> The 0th index is "streetlamp", we split the 0th index into ["s","t","r","e","e","t","l","a","m","p"], we join the new array as "streetlamp", we call on the 0th index of the string of "streetlamp" as "S" with charAt(0) + .toUpperCase(), we use the method .substring(1) to eject the first letter of the string "streetlamp" to get "treetlamp", we use the plus symbol to basically concat the two string "S" and "treetlamp", and the result "Streetlamp" is pushed into containerArray, the next INDEX (which is 1) for randomNouns1 begins, until all elements of randomNouns is exhausted. 
+        containerArray.push(array[index].split("").join("").charAt(0).toUpperCase() + array[index].substring(1))
+    } return  containerArray
+    
+}
+
+// SUCCESS!!!!
+//  PASS  ./code-challenges.test.js
+//   Divisiblity
+//   ✓ Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3 (2 ms)
+// capitalize
+//   ✓ Input array is lowercase strings, Output should be an array with every first letter capitalize
+// vowelFinder
+//   ✓ Inputted is a string, Output is the index of the FIRST vowel
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+
+
+
+
+
 
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
-
-// a) Create a test with expect statements for each of the variables provided.
-
 const vowelTester1 = "learn"
 // Expected output: 1
 const vowelTester2 = "academy"
@@ -65,5 +160,80 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
+// a) Create a test with expect statements for each of the variables provided.
+//PseudoCode: 
+// Declare a function called vowelFinder to find the index of the first vowel
+// Input / parameter is a varible that is defined as a string
+// Output should be the index value of a vowel ==> a,e,i,o,u
+
+// Describe a function that is undefined ==> triggers the yarn jest to tell us we failed to identify the function
+// it statements just tells us how the function will operate
+// expect statements tells jest what we will input and what we expect to come out 
+describe ("vowelFinder", () => {
+    it ("Inputted is a string, Output is the index of the FIRST vowel", () => {
+        expect (vowelFinder(vowelTester1)).toEqual(1)
+        expect (vowelFinder(vowelTester2)).toEqual(0)
+        expect (vowelFinder(vowelTester3)).toEqual(2)
+    })
+})
+// Good Failure:
+// FAIL  ./code-challenges.test.js
+// divisiblity
+//   ✓ Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3 (3 ms)
+// vowelFinder
+//   ✕ Inputted is a string, Output is the index of the FIRST vowel
+// ● vowelFinder › Inputted is a string, Output is the index of the FIRST vowel
+//   ReferenceError: vowelFinder is not defined
 
 // b) Create the function that makes the test pass.
+// Create function vowelFinder
+// Create a for loop that looks at all indivudual character in a string
+// Once a vowel is found log the index location 
+// the vowel should be flexible to all 5 vowels, it should not be specific on finding one vowel first
+
+// Failed attempt 1. 
+// Not sure why this didnt work out
+// const vowelFinder = (string) => {
+//     for (let index = 0 ; index < string.length ; index = index + 1) {
+//         if (string[index] === "a" && string[index] === "e" && string[index] === "i" &&  string[index] === "o" && string[index] === "u"){
+//             return string.indexOf("[index]")
+//         } else {
+
+//         }
+
+//     }
+// }
+
+//Failed attempt 2. 
+// ... return [index]
+// gave the letter of the vowel
+
+//Failed attempt 3. 
+// ... return [index].toString()
+// gave all numbers into string, when the final data type is a number
+
+
+const vowelFinder = (string) => {
+    for (let index = 0 ; index < string.length ; index = index + 1) {
+        if (string[index] === "a"){
+            return Number([index])
+        } else if (string[index] === "e") {
+            return Number([index])
+        } else if (string[index] === "i") {
+            return Number([index])
+        } else if (string[index] === "o") {
+            return Number([index])
+        } else if (string[index] === "u") {
+            return Number([index])
+        } else {
+
+        }
+    }
+}
+
+//SUCCESS!!!
+// PASS  ./code-challenges.test.js
+//   divisiblity
+//     ✓ Inputted value is an object that has a property called number, Output tells us if the number is divisible by 3 (2 ms)
+//   vowelFinder
+//     ✓ Inputted is a string, Output is the index of the FIRST vowel
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 const cohort = "Echo 2022"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ["E", "c", "h", "o", " ", "2", "0", "2", "2"]
+// b) Verify and explain: The answer is: ["E", "c", "h", "o", " ", "2", "0", "2", "2"]. The .split() method turns a string into an array, including string. .split() turns the entire string into an array, while .split("") turns every single letters and spaces into an array element as one single array.
 
 
 // --------------------2) What will this log?
@@ -21,8 +21,8 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined
+// b) Verify and explain: The answer is: undefined . Although the code block looks like a function, it is missing one characteristic the return keyword. Without a return the code block does not know what to return. All Functions have a declaration, varible name, assignment operator, parameter, arrow notation, {} brackets, function statements, and a return keyword (more returns are needed if the function has a standard for loop) 
 
 
 // --------------------3) What will this log?
@@ -30,17 +30,19 @@ const greeter = (name) => {
 const multipliedByTwo = [4, 5, 6, 7, 8].map(number => number * 2)
 // console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [8, 10, 12, 14, 16]
+// b) Verify and explain: The answer is: [8, 10, 12, 14, 16] . This code block uses syntactical sugar, which is a method that writes short hand code. .map () is a method that returns the original length of the array with or with not any modifications. We also note that parameters in the .map() doesn't use a function, so that explains why we don't see a return. 
+// as a side bar .map() , .filer(), .forEach(), emulates a for loop, as so these reiteratable methods can only work on iterable data types of strings and arrays
 
 
 // --------------------4) What will this log?
 
+
 const onlyOdds = [11, 12, 13, 14, 15].filter(number => number % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain: The answer is: [11, 13, 15] . This code block uses syntactical sugar, which is a method that writes short hand code. .filter() is a method that only accepts data values that satisfies whatever conditions is in the parameter, and, so, the OnlyOdds only logs odd numbers from the array.
 
 
 // --------------------5) What will this log?
@@ -53,8 +55,8 @@ const myCodingSkills = {
 }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Javascript"
+// b) Verify and explain: The answer is: "Javascript" . We are given an object called myCodingSkills, with properties that have an associated data value (properties and its data values are called key values pairs). The code block uses dot notation that locates key value pairs, usually it follows this order ==> myObject.properties[index] -- as such we are looking at the languages property, and the 0th index of that respective array.
 
 
 // --------------------STRETCH: What will this log?
@@ -69,5 +71,17 @@ class Learn {
 const learnStudent = new Learn("George")
 // console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 
+// ***** Without the "//""
+//   constructor("George"){
+//     this.student = "George",
+//     this.cohort = "Echo",
+//     this.year = 2022
+//   }
+// }
+
+// b) Verify and explain: The correct answer is: 
+// {cohort: "Echo"
+// student: "George"
+// year: 2022}
+// The anatomy of this class: class is a blueprint for an object, constructor allows for the creation of an object -- whatever that is in the parameter of the constructor replaces the identical parameter inside the key values; in this problem, name ,is the parameter in the constructor, so where ever name is in the key values it will be replaced by whatever the devloper decides. The this. format is a shorthand to refer to the class name. By entering George, we get all the properties back below the constructor, remembering that the orginal parameter is name, so where ever name is in the class's properties it will also be replaced with "George" as well.
